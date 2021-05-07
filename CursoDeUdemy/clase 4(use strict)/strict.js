c14ce3b54077b89ab30fa06299c2ca667db068b5
shuarz <shuarz.wolfz@gmail.com>
2021-05-07 20:00:04 -0300

    diferencia entre let y var

---
+++ CursoDeUdemy/clase 4(use strict)/strict.js
@@ -0,0 +1,6 @@
+'use strict'; //esto me obliga a decirle a la computadora que numero es una variable
+
+var numero = 30;
+
+console.log(numero);
+
---
+++ CursoDeUdemy/clase 5(diferencia entre  let y var)/varYlet.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var numero = 20;
+
+if(true)
+{
+    let numero = 40; //va a cambiar el valor de la variable dentro del bloque que se esta ejecutando
+
+    console.log(numero);
+}
+
+console.log(numero);
