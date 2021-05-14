9e4e1a129ba039e6cf435b4b1f58b2c99b07e37b
shuarz <shuarz.wolfz@gmail.com>
2021-05-14 20:21:14 -0300

    do while

---
+++ CursoDeUdemy/clase 7(operadores logicos)/do while.js
@@ -0,0 +1,7 @@
+var dato = 1;
+
+do //hace algo previo al while
+{
+    console.log("el valor es " + dato);
+    dato++;
+}while(dato <= 5);
---
+++ CursoDeUdemy/clase 7(operadores logicos)/operadores.js
@@ -0,0 +1,27 @@
+var numero = 1;
+var numero2 =5;
+
+var suma = numero2 + numero;
+
+var resta = numero + numero2;
+
+var division = numero / numero2;
+
+var multiplicacion = numero * numero2;
+
+console.log("el resultado de la suma es: " + suma);
+
+console.log("el resultado de la resta es: " + resta);
+
+console.log("el resultado de la division es: " + division);
+
+console.log("el resultado de la multiplicacion es: " + multiplicacion);
+
+// incrementos y decremento
+
+suma ++; //esto al resultado le suma uno
+console.log("se incrementa " + suma);
+
+suma --; //esto al resultado le resta uno
+console.log("descrece " + suma);
+
